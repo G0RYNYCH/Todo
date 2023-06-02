54162bc0dc00c5dc56c78789e057c6e0fe3bf1c5
Ivan Zaitsev <ivan.zaitsev@innowise-group.com>
2023-06-02 22:13:28 +0300

    list of users, todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
+import {ITodo, IUser} from "./types/types";
+import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
+import EventsExample from "./components/EventsExample";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // const users: IUser[] =[
+    //     {id: 1, name: 'name1', email: 'email1', address: {city: 'city1', street: 'street1', zipcode: 'zipcode1'}},
+    //     {id: 2, name: 'name2', email: 'email2', address: {city: 'city2', street: 'street2', zipcode: 'zipcode2'}}
+    // ]
+
+    const [users, setUsers] = useState<IUser[]>([])
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    useEffect(() => {
+        fetchUsers()
+        fetchTodos()
+    }, [])
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    async function fetchTodos() {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    return (
+        <div>
+            <EventsExample/>
+            <Card width='200px' height='200px' variant={CardVariant.outlined}>
+                <button>Button</button>
+            </Card>
+            <List
+                items={users}
+                renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
+            />
+            <List
+                items={todos}
+                renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>}
+            />
+        </div>
+    );
+};
 
 export default App;
